cc23907092aa60700daa895f9d86a550407e79dd
Yusuf Nasser <YusufNasserEng@gmail.com>
2023-08-11 16:41:02 +0300

    Add Big Countries problem solution

--- StudyPlans/30DaysPandas/BigCountries.py
+++ StudyPlans/30DaysPandas/BigCountries.py
@@ -1,5 +1,4 @@
 import pandas as pd
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    world = world.query('area >= 3000000 and population >= 25000000')
-    return world[['name', 'population', 'area']]
+    return world.loc[(world['area'] >= 3000000) | (world['population'] >= 25000000)].drop(columns=["continent", "gdp"])
