2444642fa3895282236a9fb0555665ba929b8efa
Yusuf Nasser <YusufNasserEng@gmail.com>
2023-08-14 15:50:23 +0300

    Add Valid Emails problem solution

---
+++ StudyPlans/30DaysPandas/StringMethods/ValidEmails.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    users = users[users['mail'].str.match('^[a-zA-Z][a-zA-Z0-9._-]*@leetcode\.com$')]
+    return users
