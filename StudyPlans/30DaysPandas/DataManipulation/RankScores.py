e81a4e5778261309e417918a8de333cff6ce5160
Yusuf Nasser <YusufNasserEng@gmail.com>
2023-08-18 20:02:21 +0300

    Add Rank Scores problem solution

---
+++ StudyPlans/30DaysPandas/DataManipulation/RankScores.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
+    scores['rank'] = scores['score'].rank(method='dense', ascending=False)
+    return scores.drop('id', axis=1).sort_values(by='score', ascending=False)
